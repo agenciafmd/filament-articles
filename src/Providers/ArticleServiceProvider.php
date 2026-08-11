0c0a75b7ea0ccdbde1e6adb2beef43864e750e45
João Paulo dos Santos Ferreira <joao.paulo@fmd.ag>
2026-08-11 10:14:34 -0300

    feat(providers): registra CommandServiceProvider e agenda prune

--- src/Providers/ArticleServiceProvider.php
+++ src/Providers/ArticleServiceProvider.php
@@ -24,7 +24,7 @@ final class ArticleServiceProvider extends ServiceProvider
 
     private function bootProviders(): void
     {
-        //
+        $this->app->register(CommandServiceProvider::class);
     }
 
     private function bootMigrations(): void
